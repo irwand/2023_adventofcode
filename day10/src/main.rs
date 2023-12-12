c7a9f4c503e934d56a9d85f796ea4354866beac3
Irwan Djajadi <irwan.djajadi@ni.com>
2023-12-11 21:55:48 -0500

    day10part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -51,7 +51,7 @@ fn get_next_move(from: char, pipe: char) -> (char, (i32, i32)) {
 
 fn main() {
     // pass first argument to function
-    let lines = _open_file_and_return_lines(std::env::args().nth(1).unwrap());
+    let mut lines = _open_file_and_return_lines(std::env::args().nth(1).unwrap());
 
     let mut position: (i32, i32) = (0, 0);
     for (index, line) in lines.iter().enumerate() {
@@ -60,6 +60,7 @@ fn main() {
             position = (col as i32, index as i32);
         }
     }
+    let s_coord = position;
 
     let mut paths: Vec<(usize, usize)> = Vec::new();
     let mut from = 'S';
@@ -88,4 +89,71 @@ fn main() {
         paths.len(),
         paths.len() / 2
     );
+
+    let mut paths_copy = paths.clone();
+    paths_copy.sort_by(|a, b| {
+        if a.1 != b.1 {
+            a.1.cmp(&b.1)
+        } else {
+            a.0.cmp(&b.0)
+        }
+    });
+
+    // 'S' is 'J'
+    assert!(
+        lines[s_coord.1 as usize]
+            .chars()
+            .nth(s_coord.0 as usize)
+            .unwrap()
+            == 'S'
+    );
+    lines[s_coord.1 as usize] = lines[s_coord.1 as usize].replacen("S", "J", 1);
+    println!("{}", lines[s_coord.1 as usize]);
+    let mut tiles: u32 = 0;
+    for y in 0..lines.len() {
+        let paths_this_line: Vec<usize> = paths_copy
+            .iter()
+            .filter(|i| i.1 == y)
+            .map(|i| i.0)
+            .collect();
+
+        let mut in_area = false;
+        let mut look_for = '0';
+        for x in 0..lines[y].len() {
+            if paths_this_line.contains(&x) {
+                let char_here = lines[y].chars().nth(x).unwrap();
+                if char_here == '|' {
+                    in_area = !in_area;
+                } else if char_here == '-' {
+                    if look_for == '0' {
+                        panic!(
+                            "Invalid path, char_here: {}, look_for: {}, x: {}, y: {}",
+                            char_here, look_for, x, y
+                        );
+                    }
+                } else if char_here == look_for {
+                    in_area = !in_area;
+                    look_for = '0';
+                } else if char_here == 'L' && look_for == '0' {
+                    look_for = '7';
+                } else if char_here == 'F' && look_for == '0' {
+                    look_for = 'J';
+                } else if char_here == 'J' && look_for == '7' {
+                    look_for = '0';
+                } else if char_here == '7' && look_for == 'J' {
+                    look_for = '0';
+                } else {
+                    panic!(
+                        "Invalid path, char_here: {}, look_for: {}, x: {}, y: {}",
+                        char_here, look_for, x, y
+                    );
+                }
+            } else {
+                if in_area {
+                    tiles += 1;
+                }
+            }
+        }
+    }
+    println!("Total tiles: {}", tiles);
 }
