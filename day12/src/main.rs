89bbaa2818adb90d079ea3f93715b6c48fa5da1b
Irwan Djajadi <irwan.djajadi@ni.com>
2023-12-15 21:37:58 -0500

    wip

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,4 @@
-use itertools::{Itertools, MultiProduct};
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -33,55 +33,77 @@ fn _count_consecutive_hashes(line: &String) -> Vec<u32> {
     counts
 }
 
-fn _count_question_mark(line: &String) -> u32 {
-    line.chars().filter(|&c| c == '?').count() as u32
+fn _count_char(line: &String, character: char) -> usize {
+    line.chars().filter(|&c| c == character).count() as usize
 }
 
-/// Rust version of Python's itertools.product().
-/// It returns the cartesian product of the input iterables, and it is
-/// semantically equivalent to `repeat` nested for loops.
-///
-/// # Arguments
-///
-/// * `it` - An iterator over a cloneable data structure
-/// * `repeat` - Number of repetitions of the given iterator
-pub fn product_repeat<I>(it: I, repeat: usize) -> MultiProduct<I>
-where
-    I: Iterator + Clone,
-    I::Item: Clone,
-{
-    std::iter::repeat(it).take(repeat).multi_cartesian_product()
+fn _calc_possibilities(sequence: &String, vec_num: &Vec<u32>) -> u64 {
+    let num_question_marks = _count_char(&sequence, '?');
+    let mut possibilities: u64 = 0;
+
+    let total_hashes = vec_num.iter().copied().sum::<u32>() as usize;
+    let num_hashes = _count_char(&sequence, '#');
+    let hash_needed = total_hashes - num_hashes;
+
+    for i in (0..num_question_marks).combinations(hash_needed) {
+        let dot_hash: Vec<char> = (0..num_question_marks)
+            .map(|x| if i.contains(&x) { '#' } else { '.' })
+            .collect();
+        // println!("{:?} ", dot_hash);
+        let mut sequence = sequence.clone();
+        for c in dot_hash.iter() {
+            sequence = sequence.replacen('?', &c.to_string(), 1);
+        }
+        let counts = _count_consecutive_hashes(&sequence);
+        if counts == *vec_num {
+            possibilities += 1;
+            // println!("{:04} {}", possibilities, sequence);
+        }
+    }
+    possibilities
 }
 
 fn main() {
     // pass first argument to function
     let lines = _open_file_and_return_lines(std::env::args().nth(1).unwrap());
 
-    let mut possibilities = 0;
-    for line in lines {
+    let mut possibilities_per_line_orig: Vec<u64> = Vec::new();
+    let mut possibilities_per_line_next: Vec<u64> = Vec::new();
+    for (line_num, line) in lines.iter().enumerate() {
         let mut split = line.split_whitespace();
         let sequence = split.next().unwrap().to_string();
-        let vec_num: Vec<u32> = split
+        let mut vec_num: Vec<u32> = split
             .next()
             .unwrap()
             .split(',')
             .map(|x| x.parse::<u32>().unwrap())
             .collect();
-        println!("{} {:?}", sequence, vec_num);
 
-        let num_question_marks = _count_question_mark(&sequence);
+        let possibilities = _calc_possibilities(&sequence, &vec_num);
+        println!("{} {} {:?} {}", line_num, sequence, vec_num, possibilities);
+        possibilities_per_line_orig.push(possibilities);
 
-        for i in product_repeat(['.', '#'].iter(), num_question_marks as usize) {
-            let mut sequence = sequence.clone();
-            for c in i.iter() {
-                sequence = sequence.replacen('?', &c.to_string(), 1);
-            }
-            let counts = _count_consecutive_hashes(&sequence);
-            if counts == vec_num {
-                possibilities += 1;
-                // println!("{} {}", possibilities, sequence);
-            }
-        }
+        // let sequence_copy: String = "?".to_string() + sequence.as_str();
+        // let sequence_copy: String = sequence + "?";
+        let sequence_copy = [sequence.to_owned(), sequence.to_owned()].join("?");
+        vec_num = vec_num.repeat(2);
+        let possibilities = _calc_possibilities(&sequence_copy, &vec_num);
+        println!(
+            "{} {} {:?} {}",
+            line_num, sequence_copy, vec_num, possibilities
+        );
+        possibilities_per_line_next.push(possibilities);
     }
-    println!("total possibilities {}", possibilities);
+    let mut total_possibilities: u64 = 0;
+    for (first, second) in possibilities_per_line_orig
+        .iter()
+        .zip(possibilities_per_line_next.iter())
+    {
+        let factor = second / first;
+        // let factor = second;
+        let addition = first * factor * factor * factor * factor;
+        println!("{} {} {}", first, second, addition);
+        total_possibilities += addition;
+    }
+    println!("total possibilities {}", total_possibilities);
 }
